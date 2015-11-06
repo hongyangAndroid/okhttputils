bbd0fc6ce20c295fc1519c815dee5e442911fc11
hongyangAndroid <lmj623565791@gmail.com>
2015-11-07 03:35:28 +0800

    增加sample

---
+++ sample-okhttp/src/main/java/com/zhy/sample_okhttp/MyApplication.java
@@ -0,0 +1,44 @@
+package com.zhy.sample_okhttp;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpClientManager;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+import okio.Buffer;
+
+/**
+ * Created by zhy on 15/8/25.
+ */
+public class MyApplication extends Application
+{
+    private String CER_12306 = "-----BEGIN CERTIFICATE-----\n" +
+            "MIICmjCCAgOgAwIBAgIIbyZr5/jKH6QwDQYJKoZIhvcNAQEFBQAwRzELMAkGA1UEBhMCQ04xKTAn\n" +
+            "BgNVBAoTIFNpbm9yYWlsIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MQ0wCwYDVQQDEwRTUkNBMB4X\n" +
+            "DTA5MDUyNTA2NTYwMFoXDTI5MDUyMDA2NTYwMFowRzELMAkGA1UEBhMCQ04xKTAnBgNVBAoTIFNp\n" +
+            "bm9yYWlsIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MQ0wCwYDVQQDEwRTUkNBMIGfMA0GCSqGSIb3\n" +
+            "DQEBAQUAA4GNADCBiQKBgQDMpbNeb34p0GvLkZ6t72/OOba4mX2K/eZRWFfnuk8e5jKDH+9BgCb2\n" +
+            "9bSotqPqTbxXWPxIOz8EjyUO3bfR5pQ8ovNTOlks2rS5BdMhoi4sUjCKi5ELiqtyww/XgY5iFqv6\n" +
+            "D4Pw9QvOUcdRVSbPWo1DwMmH75It6pk/rARIFHEjWwIDAQABo4GOMIGLMB8GA1UdIwQYMBaAFHle\n" +
+            "tne34lKDQ+3HUYhMY4UsAENYMAwGA1UdEwQFMAMBAf8wLgYDVR0fBCcwJTAjoCGgH4YdaHR0cDov\n" +
+            "LzE5Mi4xNjguOS4xNDkvY3JsMS5jcmwwCwYDVR0PBAQDAgH+MB0GA1UdDgQWBBR5XrZ3t+JSg0Pt\n" +
+            "x1GITGOFLABDWDANBgkqhkiG9w0BAQUFAAOBgQDGrAm2U/of1LbOnG2bnnQtgcVaBXiVJF8LKPaV\n" +
+            "23XQ96HU8xfgSZMJS6U00WHAI7zp0q208RSUft9wDq9ee///VOhzR6Tebg9QfyPSohkBrhXQenvQ\n" +
+            "og555S+C3eJAAVeNCTeMS3N/M5hzBRJAoffn3qoYdAO1Q8bTguOi+2849A==\n" +
+            "-----END CERTIFICATE-----";
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        OkHttpClientManager.getInstance().setCertificates(new InputStream[]{
+                new Buffer()
+                        .writeUtf8(CER_12306)
+                        .inputStream()});
+        OkHttpClientManager.getInstance().getOkHttpClient().setConnectTimeout(100000, TimeUnit.MILLISECONDS);
+
+
+    }
+}
