ca7395a7dd16f6d2dbf75e7e500ff907c56a434d
zengcanxiang <1372750445@qq.com>
2016-03-22 09:54:32 +0800

    Update OkHttpUtils.java

--- okhttputils/src/main/java/com/zhy/http/okhttp/OkHttpUtils.java
+++ okhttputils/src/main/java/com/zhy/http/okhttp/OkHttpUtils.java
@@ -322,6 +322,7 @@ public class OkHttpUtils
         public static final String DELETE = "DELETE";
         public static final String PUT = "PUT";
         public static final String PATCH = "PATCH";
+        public static final String POST = "POST";
     }
 }
 
