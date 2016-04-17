c75a154cf463693f0471a9c466edab109125fca7
kizure <lucifr.kin@gmail.com>
2016-04-17 16:28:23 +0800

    fix PersistentCookieStore
    cookiePrefs.getString(name,null);

--- okhttputils/src/main/java/com/zhy/http/okhttp/cookie/store/PersistentCookieStore.java
+++ okhttputils/src/main/java/com/zhy/http/okhttp/cookie/store/PersistentCookieStore.java
@@ -42,165 +42,125 @@ import okhttp3.HttpUrl;
  */
 public class PersistentCookieStore implements CookieStore
 {
-
     private static final String LOG_TAG = "PersistentCookieStore";
-    private static final String COOKIE_PREFS = "CookiePrefsFile";
-    private static final String COOKIE_NAME_PREFIX = "cookie_";
+    private static final String COOKIE_PREFS = "Cookies_Prefs";
 
-    private final HashMap<String, ConcurrentHashMap<String, Cookie>> cookies;
+    private final Map<String, ConcurrentHashMap<String, Cookie>> cookies;
     private final SharedPreferences cookiePrefs;
 
-    /**
-     * Construct a persistent cookie store.
-     *
-     * @param context Context to attach cookie store to
-     */
-    public PersistentCookieStore(Context context)
-    {
-        cookiePrefs = context.getSharedPreferences(COOKIE_PREFS, 0);
-        cookies = new HashMap<String, ConcurrentHashMap<String, Cookie>>();
+    public PersistentCookieStore(Context ct) {
+        cookiePrefs = ct.getSharedPreferences(COOKIE_PREFS, 0);
+        cookies = new HashMap<>();
 
-        // Load any previously stored cookies into the store
+        //将持久化的cookies缓存到内存中 即map cookies
         Map<String, ?> prefsMap = cookiePrefs.getAll();
-        for (Map.Entry<String, ?> entry : prefsMap.entrySet())
-        {
-            if (((String) entry.getValue()) != null && !((String) entry.getValue()).startsWith(COOKIE_NAME_PREFIX))
-            {
-                String[] cookieNames = TextUtils.split((String) entry.getValue(), ",");
-                for (String name : cookieNames)
-                {
-                    String encodedCookie = cookiePrefs.getString(COOKIE_NAME_PREFIX + name, null);
-                    if (encodedCookie != null)
-                    {
-                        Cookie decodedCookie = decodeCookie(encodedCookie);
-                        if (decodedCookie != null)
-                        {
-                            if (!cookies.containsKey(entry.getKey()))
-                                cookies.put(entry.getKey(), new ConcurrentHashMap<String, Cookie>());
-                            cookies.get(entry.getKey()).put(name, decodedCookie);
+        for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
+            String[] cookieNames = TextUtils.split((String) entry.getValue(), ",");
+            for (String name : cookieNames) {
+                String encodedCookie = cookiePrefs.getString(name, null);
+                if (encodedCookie != null) {
+                    Cookie decodedCookie = decodeCookie(encodedCookie);
+                    if (decodedCookie != null) {
+                        if (!cookies.containsKey(entry.getKey())) {
+                            cookies.put(entry.getKey(), new ConcurrentHashMap<String, Cookie>());
                         }
+                        cookies.get(entry.getKey()).put(name, decodedCookie);
                     }
                 }
-
             }
         }
     }
 
-    protected void add(HttpUrl uri, Cookie cookie)
-    {
+    protected String getCookieToken(Cookie cookie) {
+        return cookie.name() + "@" + cookie.domain();
+    }
+
+    public void add(HttpUrl url, Cookie cookie) {
         String name = getCookieToken(cookie);
 
-        if (cookie.persistent())
-        {
-            if (!cookies.containsKey(uri.host()))
-                cookies.put(uri.host(), new ConcurrentHashMap<String, Cookie>());
-            cookies.get(uri.host()).put(name, cookie);
-        }
-        // Save cookie into persistent store
-        SharedPreferences.Editor prefsWriter = cookiePrefs.edit();
-        prefsWriter.putString(uri.host(), TextUtils.join(",", cookies.get(uri.host()).keySet()));
-        prefsWriter.putString(COOKIE_NAME_PREFIX + name, encodeCookie(new SerializableHttpCookie(cookie)));
-        prefsWriter.commit();
-    }
-
-    protected String getCookieToken(Cookie cookie)
-    {
-        return cookie.name() + cookie.domain();
-    }
-
-    @Override
-    public void add(HttpUrl uri, List<Cookie> cookies)
-    {
-        for (Cookie cookie : cookies)
-        {
-            add(uri, cookie);
-        }
-    }
-
-    @Override
-    public List<Cookie> get(HttpUrl uri)
-    {
-        ArrayList<Cookie> ret = new ArrayList<Cookie>();
-        if (cookies.containsKey(uri.host()))
-        {
-            Collection<Cookie> cookies = this.cookies.get(uri.host()).values();
-            for (Cookie cookie : cookies)
-            {
-                if (isCookieExpired(cookie))
-                {
-                    remove(uri, cookie);
-                } else
-                {
-                    ret.add(cookie);
-                }
+        //将cookies缓存到内存中 如果缓存过期 就重置此cookie
+        if (!cookie.persistent()) {
+            if (!cookies.containsKey(url.host())) {
+                cookies.put(url.host(), new ConcurrentHashMap<String, Cookie>());
+            }
+            cookies.get(url.host()).put(name, cookie);
+        } else {
+            if (cookies.containsKey(url.host())) {
+                cookies.get(url.host()).remove(name);
             }
         }
 
+        //将cookies持久化到本地
+        SharedPreferences.Editor prefsWriter = cookiePrefs.edit();
+        prefsWriter.putString(url.host(), TextUtils.join(",", cookies.get(url.host()).keySet()));
+        prefsWriter.putString(name, encodeCookie(new SerializableOkHttpCookies(cookie)));
+        prefsWriter.apply();
+    }
+
+    @Override
+    public void add(HttpUrl uri, List<Cookie> cookies) {
+        for(Cookie cookie:cookies)
+        {
+            add(uri,cookie);
+        }
+    }
+
+    public List<Cookie> get(HttpUrl url) {
+        ArrayList<Cookie> ret = new ArrayList<>();
+        if (cookies.containsKey(url.host()))
+            ret.addAll(cookies.get(url.host()).values());
         return ret;
     }
 
-    private static boolean isCookieExpired(Cookie cookie)
-    {
-        return cookie.expiresAt() < System.currentTimeMillis();
-    }
-
-    @Override
-    public boolean removeAll()
-    {
+    public boolean removeAll() {
         SharedPreferences.Editor prefsWriter = cookiePrefs.edit();
         prefsWriter.clear();
-        prefsWriter.commit();
+        prefsWriter.apply();
         cookies.clear();
         return true;
     }
 
-
-    @Override
-    public boolean remove(HttpUrl uri, Cookie cookie)
-    {
+    public boolean remove(HttpUrl url, Cookie cookie) {
         String name = getCookieToken(cookie);
 
-        if (cookies.containsKey(uri.host()) && cookies.get(uri.host()).containsKey(name))
-        {
-            cookies.get(uri.host()).remove(name);
+        if (cookies.containsKey(url.host()) && cookies.get(url.host()).containsKey(name)) {
+            cookies.get(url.host()).remove(name);
 
             SharedPreferences.Editor prefsWriter = cookiePrefs.edit();
-            if (cookiePrefs.contains(COOKIE_NAME_PREFIX + name))
-            {
-                prefsWriter.remove(COOKIE_NAME_PREFIX + name);
+            if (cookiePrefs.contains(name)) {
+                prefsWriter.remove(name);
             }
-            prefsWriter.putString(uri.host(), TextUtils.join(",", cookies.get(uri.host()).keySet()));
-            prefsWriter.commit();
+            prefsWriter.putString(url.host(), TextUtils.join(",", cookies.get(url.host()).keySet()));
+            prefsWriter.apply();
 
             return true;
-        } else
-        {
+        } else {
             return false;
         }
     }
 
-    @Override
-    public List<Cookie> getCookies()
-    {
-        ArrayList<Cookie> ret = new ArrayList<Cookie>();
+    public List<Cookie> getCookies() {
+        ArrayList<Cookie> ret = new ArrayList<>();
         for (String key : cookies.keySet())
             ret.addAll(cookies.get(key).values());
 
         return ret;
     }
 
-
-    protected String encodeCookie(SerializableHttpCookie cookie)
-    {
+    /**
+     * cookies 序列化成 string
+     *
+     * @param cookie 要序列化的cookie
+     * @return 序列化之后的string
+     */
+    protected String encodeCookie(SerializableOkHttpCookies cookie) {
         if (cookie == null)
             return null;
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        try
-        {
+        try {
             ObjectOutputStream outputStream = new ObjectOutputStream(os);
             outputStream.writeObject(cookie);
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             Log.d(LOG_TAG, "IOException in encodeCookie", e);
             return null;
         }
@@ -208,20 +168,22 @@ public class PersistentCookieStore implements CookieStore
         return byteArrayToHexString(os.toByteArray());
     }
 
-    protected Cookie decodeCookie(String cookieString)
-    {
+    /**
+     * 将字符串反序列化成cookies
+     *
+     * @param cookieString cookies string
+     * @return cookie object
+     */
+    protected Cookie decodeCookie(String cookieString) {
         byte[] bytes = hexStringToByteArray(cookieString);
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
         Cookie cookie = null;
-        try
-        {
+        try {
             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-            cookie = ((SerializableHttpCookie) objectInputStream.readObject()).getCookie();
-        } catch (IOException e)
-        {
+            cookie = ((SerializableOkHttpCookies) objectInputStream.readObject()).getCookies();
+        } catch (IOException e) {
             Log.d(LOG_TAG, "IOException in decodeCookie", e);
-        } catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             Log.d(LOG_TAG, "ClassNotFoundException in decodeCookie", e);
         }
 
@@ -229,20 +191,16 @@ public class PersistentCookieStore implements CookieStore
     }
 
     /**
-     * Using some super basic byte array &lt;-&gt; hex conversions so we don't have to rely on any
-     * large Base64 libraries. Can be overridden if you like!
+     * 二进制数组转十六进制字符串
      *
      * @param bytes byte array to be converted
      * @return string containing hex values
      */
-    protected String byteArrayToHexString(byte[] bytes)
-    {
+    protected String byteArrayToHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder(bytes.length * 2);
-        for (byte element : bytes)
-        {
+        for (byte element : bytes) {
             int v = element & 0xff;
-            if (v < 16)
-            {
+            if (v < 16) {
                 sb.append('0');
             }
             sb.append(Integer.toHexString(v));
@@ -251,17 +209,15 @@ public class PersistentCookieStore implements CookieStore
     }
 
     /**
-     * Converts hex values from strings to byte arra
+     * 十六进制字符串转二进制数组
      *
      * @param hexString string of hex-encoded values
      * @return decoded byte array
      */
-    protected byte[] hexStringToByteArray(String hexString)
-    {
+    protected byte[] hexStringToByteArray(String hexString) {
         int len = hexString.length();
         byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2)
-        {
+        for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4) + Character.digit(hexString.charAt(i + 1), 16));
         }
         return data;
