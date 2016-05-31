75fd22319a02bd7770c0c3ece8465d5be4bb5e05
wuxiang <wuxiang>
2016-06-01 01:37:06 +0800

    添加请求的CacheControl方法

--- okhttputils/src/main/java/com/zhy/http/okhttp/request/RequestCall.java
+++ okhttputils/src/main/java/com/zhy/http/okhttp/request/RequestCall.java
@@ -6,6 +6,7 @@ import com.zhy.http.okhttp.callback.Callback;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.CacheControl;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,8 +16,7 @@ import okhttp3.Response;
  * Created by zhy on 15/12/15.
  * 对OkHttpRequest的封装，对外提供更多的接口：cancel(),readTimeOut()...
  */
-public class RequestCall
-{
+public class RequestCall {
     private OkHttpRequest okHttpRequest;
     private Request request;
     private Call call;
@@ -24,35 +24,38 @@ public class RequestCall
     private long readTimeOut;
     private long writeTimeOut;
     private long connTimeOut;
+    private CacheControl cacheControl;
 
     private OkHttpClient clone;
 
-    public RequestCall(OkHttpRequest request)
-    {
+    public RequestCall(OkHttpRequest request) {
         this.okHttpRequest = request;
     }
-    public RequestCall readTimeOut(long readTimeOut)
-    {
+
+    public RequestCall readTimeOut(long readTimeOut) {
         this.readTimeOut = readTimeOut;
         return this;
     }
-    public RequestCall writeTimeOut(long writeTimeOut)
-    {
+
+    public RequestCall writeTimeOut(long writeTimeOut) {
         this.writeTimeOut = writeTimeOut;
         return this;
     }
-    public RequestCall connTimeOut(long connTimeOut)
-    {
+
+    public RequestCall connTimeOut(long connTimeOut) {
         this.connTimeOut = connTimeOut;
         return this;
     }
 
-    public Call buildCall(Callback callback)
-    {
+    public RequestCall cacheControl(CacheControl cacheControl) {
+        this.cacheControl = cacheControl;
+        return this;
+    }
+
+    public Call buildCall(Callback callback) {
         request = generateRequest(callback);
 
-        if (readTimeOut > 0 || writeTimeOut > 0 || connTimeOut > 0)
-        {
+        if (readTimeOut > 0 || writeTimeOut > 0 || connTimeOut > 0) {
             readTimeOut = readTimeOut > 0 ? readTimeOut : OkHttpUtils.DEFAULT_MILLISECONDS;
             writeTimeOut = writeTimeOut > 0 ? writeTimeOut : OkHttpUtils.DEFAULT_MILLISECONDS;
             connTimeOut = connTimeOut > 0 ? connTimeOut : OkHttpUtils.DEFAULT_MILLISECONDS;
@@ -64,55 +67,48 @@ public class RequestCall
                     .build();
 
             call = clone.newCall(request);
-        } else
-        {
+        } else if (null != cacheControl) {
+            request = request.newBuilder().cacheControl(cacheControl).build();
+            call = OkHttpUtils.getInstance().getOkHttpClient().newCall(request);
+        } else {
             call = OkHttpUtils.getInstance().getOkHttpClient().newCall(request);
         }
         return call;
     }
 
-    private Request generateRequest(Callback callback)
-    {
+    private Request generateRequest(Callback callback) {
         return okHttpRequest.generateRequest(callback);
     }
 
-    public void execute(Callback callback)
-    {
+    public void execute(Callback callback) {
         buildCall(callback);
 
-        if (callback != null)
-        {
+        if (callback != null) {
             callback.onBefore(request);
         }
 
         OkHttpUtils.getInstance().execute(this, callback);
     }
 
-    public Call getCall()
-    {
+    public Call getCall() {
         return call;
     }
 
-    public Request getRequest()
-    {
+    public Request getRequest() {
         return request;
     }
 
-    public OkHttpRequest getOkHttpRequest()
-    {
+    public OkHttpRequest getOkHttpRequest() {
         return okHttpRequest;
     }
 
-    public Response execute() throws IOException
-    {
+    public Response execute() throws IOException {
         buildCall(null);
         return call.execute();
     }
 
-    public void cancel()
-    {
-        if (call != null)
-        {
+    public void cancel() {
+        if (call != null) {
             call.cancel();
         }
     }
