222517665febfd85a68baad904de4a6924bb1780
huangxy <630686026@qq.com>
2016-07-18 00:25:52 +0800

    细化并简化了回调接口、支持返回String、User、List<User>
    细化并简化了回调接口、支持返回String、User、List<User>

---
+++ XHttpCallBack.java
@@ -0,0 +1,49 @@
+package com.administrator.ticat.AsyncTask;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.$Gson$Types;
+import com.zhy.http.okhttp.callback.Callback;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+/**
+ * Created by Git@Smark on 2016/7/17.
+ * 简化接口，支持返回String、Entity、List<Entity>
+ */
+public abstract class XHttpCallBack<T> extends Callback<T> {
+
+    @Override
+    public T parseNetworkResponse(Response response, int id) throws Exception {
+        String result = response.body().string();
+        if (getClass().getGenericSuperclass() == XHttpCallBack.class){
+            return (T)result;// 默认返回String
+        }
+        Type type = $Gson$Types.canonicalize(((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+        try {
+            return new Gson().fromJson(result, type);
+        }catch (Exception e){
+            onParser(e, id);
+        }
+        return null;
+    }
+
+    @Override
+    public void onResponse(T response, int id) {
+        onSuccess(response, id);
+    }
+
+    @Override
+    public void onError(Call call, Exception e, int id) {
+        System.out.println("网络请求错误，统一处理"+e);
+    }
+
+    public void onParser(Exception e, int id){
+        System.out.println("数据解析错误，统一处理"+e);
+    }
+
+    public abstract void onSuccess(T result, int id);
+}
---
+++ XHttpUtilsDemo.java
@@ -0,0 +1,64 @@
+package com.administrator.ticat.AsyncTask;
+
+import com.administrator.ticat.Entity.User;
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.List;
+
+/**
+ * Created by Git@Smark on 2016/7/17.
+ */
+public class XHttpUtilsDemo {
+
+    public XHttpUtilsDemo(){
+
+        String url = "https://github.com/GitSmark";
+
+        OkHttpUtils
+                .get()
+                .url(url)
+                .addParams("param1", "value")
+                .addParams("param2", "value")
+                .build()
+                .execute(new XHttpCallBack() {
+
+                    @Override
+                    public void onSuccess(Object result, int id) {
+                        //Success
+                    }
+                });
+
+        OkHttpUtils
+                .post()
+                .url(url)
+                .addParams("param1", "value")
+                .addParams("param2", "value")
+                .build()
+                .execute(new XHttpCallBack<User>() {
+
+                    @Override
+                    public void onSuccess(User result, int id) {
+                        //Success
+                    }
+                });
+
+        OkHttpUtils
+                .post()
+                .url(url)
+                .addParams("param1", "value")
+                .addParams("param2", "value")
+                .build()
+                .execute(new XHttpCallBack<List<User>>() {
+
+                    @Override
+                    public void onSuccess(List<User> result, int id) {
+                        //Success
+                    }
+
+                    //@Override
+                    //public void onParser(Exception e, int id) {
+                    //   super.onParser(e, id);
+                    //}
+                });
+    }
+}
