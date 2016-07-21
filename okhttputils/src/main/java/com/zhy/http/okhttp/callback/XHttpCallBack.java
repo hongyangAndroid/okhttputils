71a01a5e8a1427bd1ec6556e8f29486bf42930fb
huangxy <630686026@qq.com>
2016-07-21 21:20:01 +0800

    Create XHttpCallBack.java
    新增泛型序列化回调接口，简化了接口，支持返回String、Array、Entity、List<T>、BaseEntity<T>等等，支持基类预处理

---
+++ okhttputils/src/main/java/com/zhy/http/okhttp/callback/XHttpCallBack.java
@@ -0,0 +1,70 @@
+package com.zhy.http.okhttp.callback;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.$Gson$Types;
+import com.zhy.http.okhttp.callback.Callback;
+
+import org.json.JSONException;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+/**
+ * Created by Git@Smark on 2016/7/17.
+ * 简化接口，支持返回String、Array、Entity、List<T>、BaseEntity<T>等等
+ */
+public abstract class XHttpCallBack<T> extends Callback<T> {
+
+    @Override
+    public T parseNetworkResponse(Response response, int id) throws Exception {
+        String result = response.body().string();
+        try {
+            result = unBunding(result);
+        }catch (Exception e){
+            onParser(e, id);
+            return null;
+        }
+        if (getClass().getGenericSuperclass() == XHttpCallBack.class){
+            return (T)result;// 默认返回String
+        }
+        if(((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0] == String.class){
+            return (T)result;// String类型直接返回
+        }
+        try {
+            Type type = $Gson$Types.canonicalize(((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+            return new Gson().fromJson(result, type);
+        }catch (Exception e){
+            onParser(e, id);
+        }
+        return null;
+    }
+
+    @Override
+    public void onResponse(T response, int id) {
+        if (response != null) onSuccess(response, id);
+    }
+
+    public void onParser(Exception e, int id){// 数据解析错误，统一处理
+        e.printStackTrace();
+    }
+
+    @Override
+    public void onError(Call call, Exception e, int id) {// 网络请求错误，统一处理
+        e.printStackTrace();
+    }
+
+    public String unBunding(String json) throws JSONException {
+        // 很多情况下返回的json是这种格式的:{"code": 0,"msg": "success","data": [{"username": "huangxy","password": "123456",...}]}, 统一处理
+        // 这里只适用于处理获取一个返回Json串，如果不需要统一处理建议定义一个泛型基类直接返回，参考BaseEntity<T>
+        // 如果要同时获取多个data1、data2...，建议直接Entity返回
+        // JSONArray array = new JSONArray(json);
+        // JSONObject obj = new JSONObject(json);
+        // return obj.getString("data");
+        return json;
+    }
+
+    public abstract void onSuccess(T result, int id);
+}
