5600c98ebd50db75712c5441a9b503f5b45b975a
huangxy <630686026@qq.com>
2016-08-08 23:12:49 +0800

    Update XHttpCallBack.java

--- okhttputils/src/main/java/com/zhy/http/okhttp/callback/XHttpCallBack.java
+++ okhttputils/src/main/java/com/zhy/http/okhttp/callback/XHttpCallBack.java
@@ -20,11 +20,8 @@ public abstract class XHttpCallBack<T> extends Callback<T> {
 
     @Override
     public T parseNetworkResponse(Response response, int id) throws Exception {
-        String result = response.body().string();
-        try {
-            result = unBunding(result);
-        }catch (Exception e){
-            onParser(e, id);
+        String result = unBunding(response.body().string());
+        if (result == null){
             return null;
         }
         if (getClass().getGenericSuperclass() == XHttpCallBack.class){
@@ -33,13 +30,19 @@ public abstract class XHttpCallBack<T> extends Callback<T> {
         if(((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0] == String.class){
             return (T)result;// String类型直接返回
         }
-        try {
-            Type type = $Gson$Types.canonicalize(((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
-            return new Gson().fromJson(result, type);
-        }catch (Exception e){
-            onParser(e, id);
-        }
-        return null;
+        Type type = $Gson$Types.canonicalize(((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+        return new Gson().fromJson(result, type);
+    }
+
+    //注意：非UI线程，可执行耗时操作
+    public String unBunding(String json) throws JSONException {
+        // 很多情况下返回的json是这种格式的:{"code": 0,"msg": "上传成功","data": [{"id": 1,"name": "huangxy",...}]}, 统一处理
+        // 这里只适用于处理获取一个返回Json串，如果不需要统一处理建议定义一个泛型基类直接返回，参考BaseEntity<T>
+        // 如果要同时获取多个data1、data2...，建议直接Entity返回
+        // JSONArray array = new JSONArray(json);
+        // JSONObject obj = new JSONObject(json);
+        // return obj.getString("data");
+        return json;
     }
 
     @Override
@@ -47,23 +50,21 @@ public abstract class XHttpCallBack<T> extends Callback<T> {
         if (response != null) onSuccess(response, id);
     }
 
-    public void onParser(Exception e, int id){// 数据解析错误，统一处理
-        e.printStackTrace();
-    }
-
     @Override
-    public void onError(Call call, Exception e, int id) {// 网络请求错误，统一处理
+    public void onError(Call call, Exception e, int id) {
+        if (e instanceof JSONException  || e instanceof JsonIOException || e instanceof JsonSyntaxException){
+            onParser(e, id);
+            return;
+        }
+        // 网络请求错误，统一处理
+        // ToastUtils.Show(R.drawable.ic_close, R.string.error_request, Toast.LENGTH_LONG);
         e.printStackTrace();
     }
 
-    public String unBunding(String json) throws JSONException {
-        // 很多情况下返回的json是这种格式的:{"code": 0,"msg": "success","data": [{"username": "huangxy","password": "123456",...}]}, 统一处理
-        // 这里只适用于处理获取一个返回Json串，如果不需要统一处理建议定义一个泛型基类直接返回，参考BaseEntity<T>
-        // 如果要同时获取多个data1、data2...，建议直接Entity返回
-        // JSONArray array = new JSONArray(json);
-        // JSONObject obj = new JSONObject(json);
-        // return obj.getString("data");
-        return json;
+    // 数据解析错误，统一处理
+    public void onParser(Exception e, int id){
+        // ToastUtils.Show(R.drawable.ic_close, R.string.error_parser, Toast.LENGTH_LONG);
+        e.printStackTrace();
     }
 
     public abstract void onSuccess(T result, int id);
